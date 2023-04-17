8b2834521f2cdbe6dcd3e29bb0d4bb6356e4c201
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-04-17 10:24:18 -0300

    fist is now first

--- App.js
+++ App.js
@@ -12,32 +12,32 @@ export default function App() {
 
   function calculator() {
     const splitNumbers = currentNumber.split(' ')
-    const fistNumber = parseFloat(splitNumbers[0])
+    const firstNumber = parseFloat(splitNumbers[0])
     const lastNumber = parseFloat(splitNumbers[2])
     const operator = splitNumbers[1]
 
     // Faz ação referente tecla pressionada
     switch (operator) {
       case '+':
-        setCurrentNumber((fistNumber + lastNumber).toString())
+        setCurrentNumber((firstNumber + lastNumber).toString())
         return
       case '-':
-        setCurrentNumber((fistNumber - lastNumber).toString())
+        setCurrentNumber((firstNumber - lastNumber).toString())
         return
       case 'x':
-        setCurrentNumber((fistNumber * lastNumber).toString())
+        setCurrentNumber((firstNumber * lastNumber).toString())
         return
       case '/':
-        setCurrentNumber((fistNumber / lastNumber).toString())
+        setCurrentNumber((firstNumber / lastNumber).toString())
         return
       case '^2':
-        setCurrentNumber((Math.pow(fistNumber,2)).toString())
+        setCurrentNumber((Math.pow(firstNumber,2)).toString())
         return
       case '^3':
-      setCurrentNumber((Math.pow(fistNumber,3)).toString())
+      setCurrentNumber((Math.pow(firstNumber,3)).toString())
       return
       case '^':
-      setCurrentNumber((Math.pow(fistNumber, lastNumber)).toString())
+      setCurrentNumber((Math.pow(firstNumber, lastNumber)).toString())
       return
     }
   }
