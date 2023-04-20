25a857ced9a57125df24adcc66b21805c06aeb7c
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-04-20 09:45:52 -0300

    case statement instead of if

--- App.js
+++ App.js
@@ -1,89 +1,104 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState } from 'react';
 import { Text, View, TouchableOpacity, Button } from 'react-native';
-import styles from './src/Style'
+import styles from './src/Style';
 
 export default function App() {
+  const buttons = [
+    'LIMPAR',
+    'DEL',
+    '%',
+    '/',
+    7,
+    8,
+    9,
+    'x',
+    4,
+    5,
+    6,
+    '-',
+    1,
+    2,
+    3,
+    '+',
+    0,
+    '.',
+    '+/-',
+    '=',
+    '^ 3',
+    '^ 2',
+    '^',
+  ];
 
-  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "x", 
-  4, 5, 6, '-', 1, 2, 3, '+', 
-  0, '.', '+/-', '=','^ 3', '^ 2','^',]
-
-  const [currentNumber, setCurrentNumber] = useState("")
-  const [lastNumber, setLastNumber] = useState("")
-
+  const [currentNumber, setCurrentNumber] = useState('');
+  const [lastNumber, setLastNumber] = useState('');
 
   function calculator() {
-    const splitNumbers = currentNumber.split(' ')
-     let operator = ' '
-    let actualNumber = 0
+    const splitNumbers = currentNumber.split(' ');
+    let operator = ' ';
+    let actualNumber = 0;
     let newNumber = parseFloat(splitNumbers[0]);
-    let index = 1
+    let index = 1;
     while (index <= splitNumbers.length - 1) {
-       operator = splitNumbers[index]
-       actualNumber = parseFloat(splitNumbers[index+1])
+      operator = splitNumbers[index];
+      actualNumber = parseFloat(splitNumbers[index + 1]);
 
-    //Verifica se o número é um número
-    if (!isNaN(actualNumber)){
+      //Verifica se o número é um número
+      if (!isNaN(actualNumber)) {
         // Faz ação referente tecla pressionada
-    switch (operator) {
-      case '+':
-        newNumber = (newNumber + actualNumber)
-        break;
-      case '-':
-        newNumber = (newNumber - actualNumber)
-       break;
-      case 'x':
-        newNumber = (newNumber * actualNumber)
-       break;
-      case '/':
-        newNumber = (newNumber / actualNumber)
-        break;
-      case '^':
-     newNumber = Math.pow(newNumber, actualNumber)
-     break;
-    }
-      
-  index = index +2;
-    } else{
-    return
-    }
-     
+        switch (operator) {
+          case '+':
+            newNumber = newNumber + actualNumber;
+            break;
+          case '-':
+            newNumber = newNumber - actualNumber;
+            break;
+          case 'x':
+            newNumber = newNumber * actualNumber;
+            break;
+          case '/':
+            newNumber = newNumber / actualNumber;
+            break;
+          case '^':
+            newNumber = Math.pow(newNumber, actualNumber);
+            break;
+        }
+
+        index = index + 2;
+      } else {
+        return;
+      }
     }
 
-  setCurrentNumber(newNumber.toString())
-   
+    setCurrentNumber(newNumber.toString());
   }
 
   function handleInput(buttonPressed) {
-    if (buttonPressed === '+' | buttonPressed === "-" | buttonPressed === "x" | buttonPressed === "/"
-     | buttonPressed === '^' ) {
-      setCurrentNumber(currentNumber + " " + buttonPressed + " ")
-      return
-    }else if( buttonPressed === '^ 2' | buttonPressed === '^ 3'){
-setCurrentNumber(currentNumber + " " + buttonPressed)
-      return
-    }
     switch (buttonPressed) {
+      case '+': case '-': case 'x': case '/':case '^':
+        setCurrentNumber(currentNumber + ' ' + buttonPressed + ' ');
+        return;
+      case '^ 2': case '^ 3':
+        setCurrentNumber(currentNumber + ' ' + buttonPressed);
+        return;
       case 'DEL':
-        setCurrentNumber(currentNumber.substring(0, (currentNumber.length - 1)))
-        return
+        setCurrentNumber(currentNumber.substring(0, currentNumber.length - 1));
+        return;
       case 'LIMPAR': // Limpa todo o conteúdo
-        setLastNumber("")
-        setCurrentNumber("")
-        return
+        setLastNumber('');
+        setCurrentNumber('');
+        return;
       case '=':
-        setLastNumber(currentNumber + " = ")
-        calculator()
-        return
+        setLastNumber(currentNumber + ' = ');
+        calculator();
+        return;
       case '+/-':
-        setCurrentNumber((-1 * currentNumber).toString())
-        return
+        setCurrentNumber((-1 * currentNumber).toString());
+        return;
     }
 
-    setCurrentNumber(currentNumber + buttonPressed)
+    setCurrentNumber(currentNumber + buttonPressed);
   }
-
 
   return (
     <View style={styles.container}>
@@ -92,18 +107,33 @@ setCurrentNumber(currentNumber + " " + buttonPressed)
         <Text style={styles.resultText}>{currentNumber}</Text>
       </View>
 
-      
       <View style={styles.buttons}>
-
         {buttons.map((button) =>
-          button === '=' ? // Mapeamento do botão =
-            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#FC3AF3' }]}>
-              <Text style={[styles.textButton, { color: "white", fontSize: 30 }]}>{button}</Text>
+          button === '=' ? ( // Mapeamento do botão =
+            <TouchableOpacity
+              onPress={() => handleInput(button)}
+              key={button}
+              style={[styles.button, { backgroundColor: '#FC3AF3' }]}>
+              <Text
+                style={[styles.textButton, { color: 'white', fontSize: 30 }]}>
+                {button}
+              </Text>
             </TouchableOpacity>
-            : // Mapeamento dos outros botões
-            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-              <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'white' : '#2E2EFE' }]}>{button}</Text>
+          ) : (
+            // Mapeamento dos outros botões
+            <TouchableOpacity
+              onPress={() => handleInput(button)}
+              key={button}
+              style={styles.button}>
+              <Text
+                style={[
+                  styles.textButton,
+                  { color: typeof button === 'number' ? 'white' : '#2E2EFE' },
+                ]}>
+                {button}
+              </Text>
             </TouchableOpacity>
+          )
         )}
       </View>
     </View>
